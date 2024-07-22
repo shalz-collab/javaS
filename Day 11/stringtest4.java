5669467c68f60c3011f87452f7b6b92aa61b0f25
shalz-collab <shalinim1434.sse@saveetha.com>
2024-07-22 21:29:44 +0530

    Add files via upload

---
+++ Day 11/stringtest4.java
@@ -0,0 +1,13 @@
+class stringtest4
+{
+	public static void main(String arg[]) 
+	{
+		String s1 = "I Love java";
+		String s2;
+		int i;
+		s2=s1.substring(3,5);
+		System.out.println(s2);
+                  i=s1.indexOf('o');
+		System.out.println(i);
+			}
+}
---
+++ Day 11/stringtest7.java
@@ -0,0 +1,13 @@
+class stringtest7
+{
+	public static void main(String arg[]) 
+	{
+		String s1 = "I Love java";
+		String s2;
+		int i;
+		s2=s1.substring(5);
+		System.out.println(s2);
+                
+  
+			}
+}
