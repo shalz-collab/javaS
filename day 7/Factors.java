fdaa62fb581854a65d67af52ed8763d4c1de459b
shalz-collab <shalinim1434.sse@saveetha.com>
2024-07-22 20:38:29 +0530

    Add files via upload

---
+++ day 7/Factors.java
@@ -0,0 +1,16 @@
+class Factors
+ {
+    public static void main(String[] args) 
+{
+        
+        int number = 60;
+        System.out.println("Factors of " + number + " are: ");
+        for (int i = 1; i <= number; ++i) 
+{
+            if (number % i == 0) 
+{
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
