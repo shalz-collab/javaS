90b192dfddba7d67fefa3f04245286512a674776
shalz-collab <shalinim1434.sse@saveetha.com>
2024-07-22 21:20:24 +0530

    Add files via upload

---
+++ Day 11/stringtest5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class StringTest5 {
+
+    public static void main(String[] args) 
+{
+       Scanner scanner = new Scanner(System.in);
+     string x=sc.nextline();
+     string y=sc.nextline();
+
+        if(x.equals(y))
+{
+        System.out.println("giveen string ARE equal");
+}
+else
+{
+System.out.println("giveen string ARE not equal");
+
+}
+
+    }
+}
